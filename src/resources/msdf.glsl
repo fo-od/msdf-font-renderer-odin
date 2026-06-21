b8fc16972ef8491b12a2351e1215fceae045d43e
fo-od <fooduh@proton.me>
2026-06-20 20:53:25 -0700

    Update msdf.glsl

--- src/resources/msdf.glsl
+++ src/resources/msdf.glsl
@@ -1,12 +1,12 @@
 #version 330 core
 
+// modified from https://github.com/Blatko1/awesome-msdf#basic-msdf-usage
 in vec2 fragTexCoord;
 in vec4 fragColor;
 out vec4 finalColor;
 
-uniform sampler2D texture0;
-uniform float scale = 1;
-uniform vec4 bgColor = vec4(0.0, 0.0, 0.0, 0.0); // fallback color
+uniform sampler2D tex;
+uniform float pxRange = 2.0;
 uniform vec4 fgColor = vec4(1.0, 1.0, 1.0, 1.0); // fallback color
 
 float median(float r, float g, float b) {
@@ -14,11 +14,13 @@ float median(float r, float g, float b) {
 }
 
 float screenPxRange() {
-    return (32 * scale) / 32 * 2;
+    vec2 unitRange = vec2(pxRange) / vec2(textureSize(tex, 0));
+    vec2 screenTexSize = vec2(1.0) / fwidth(fragTexCoord);
+    return max(0.5 * dot(unitRange, screenTexSize), 1.0);
 }
 
 void main() {
-    vec3 msd = texture(texture0, fragTexCoord).rgb;
+    vec3 msd = texture(tex, fragTexCoord).rgb;
     float sd = median(msd.r, msd.g, msd.b);
     float screenPxDistance = screenPxRange() * (sd - 0.5);
     float opacity = clamp(screenPxDistance + 0.5, 0.0, 1.0);
